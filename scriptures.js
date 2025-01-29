406d95bc00832cc0f3dc285f5f7e06b9112fc4e4
youneedanemail <120159652+youneedanemail@users.noreply.github.com>
2025-01-29 12:42:17 -0700

    video 2

--- scriptures.js
+++ scriptures.js
@@ -1,30 +1,57 @@
 const Scriptures = (function () {
   const REQUEST_GET = "GET";
-  const URL_BASE = "https://scriptures.byu.edu/mapscrp/";
+  const REQUEST_STATUS_OK = 200;
+  const REQUEST_STATUS_ERROR = 400;
+  const URL_BASE = "https://scriptures.byu.edu/mapscrip/";
   const URL_BOOKS = `${URL_BASE}model/books.php`;
   const URL_VOLUMES = `${URL_BASE}model/volumes.php`;
 
-  function init() {
-    let request = new XMLHttpRequest();
+  function getJSONRequest(url, successCallback) {
+    const request = new XMLHttpRequest();
 
     function handleAjaxError() {
       console.log("There was an AJAX problem.");
     }
 
     function handleAjaxResponse() {
-      console.log("Request received.");
+      if (
+        request.status >= REQUEST_STATUS_OK &&
+        request.status < REQUEST_STATUS_ERROR
+      ) {
+        const data = JSON.parse(request.response);
+
+        successCallback(data);
+      } else {
+        handleAjaxError();
+      }
     }
 
-    request.open(REQUEST_GET, URL_BOOKS, true);
+    request.open(REQUEST_GET, url, true);
     request.onload = handleAjaxResponse;
     request.onerror = handleAjaxError;
     request.send();
   }
 
+  function init() {
+    let books;
+    let volumes;
+
+    getJSONRequest(URL_BOOKS, (json) => {
+      books = json;
+      console.log(books);
+    });
+    getJSONRequest(URL_VOLUMES, (json) => {
+      volumes = json;
+      console.log(volumes);
+    });
+  }
+
   return {
     init,
-  }; // if you don't return anything, it will by default return undefined
+  };
 })();
+
+// if you don't return anything, it will by default return undefined
 
 // need to create a module to avoide poluting global name space
 
